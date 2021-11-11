c80d974ecada743380f27b3496c648f9889a8e5b
oberon1984 <89933511+oberon1984@users.noreply.github.com>
2021-11-11 21:03:26 +0200

    update commit

--- MemoryGame.py
+++ MemoryGame.py
@@ -10,24 +10,30 @@ class MemoryGame(Game):
     #     super().__init__(game_type, difficulty)
     #     self.game_type = game_type
     #     self.difficulty = difficulty
+
     def play(self, difficulty):
-        generate_sequence(difficulty)
+        print(is_list_equal(generate_sequence(difficulty), get_list_from_user(difficulty)))
 
 
 def generate_sequence(difficulty):
     print("\n" * 1000)
-    random_num = []
+    random_num_list = []
     for i in range(1, difficulty + 1):
-        random_num.append(randrange(1, 102))
-    print(*random_num, sep=', ')
+        random_num_list.append(randrange(1, 102))
+    print(*random_num_list, sep=', ')
     time.sleep(0.7)
     print("\n" * 1000)
-    return random_num
+    return random_num_list
 
 
 def get_list_from_user(difficulty):
     user_num_list = []
     for i in range(1, difficulty + 1):
-        user_num = input(f"Please enter one number you remember and press 'Enter'")
+        user_num = int(input(f"Please enter one number you remember and press 'Enter':\n"))
         user_num_list.append(user_num)
     return user_num_list
+
+
+def is_list_equal(random_num_list, user_num_list):
+    is_equal = random_num_list == user_num_list
+    return is_equal
