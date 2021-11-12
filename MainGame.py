85c572ef53187cd1739c17756dc6828e3167b7dc
oberon1984 <89933511+oberon1984@users.noreply.github.com>
2021-11-12 09:55:07 +0200

    update commit

--- MainGame.py
+++ MainGame.py
@@ -1,6 +1,7 @@
 from Live import player_name, welcome, load_game
 from GuessGame import *
 from MemoryGame import *
+from CurrencyRouletteGame import *
 
 welcome(player_name())
 game = load_game()
@@ -8,6 +9,6 @@ if game.game_type == "1":
     MemoryGame(game.game_type, game.difficulty).play(int(game.difficulty))
 elif game.game_type == "2":
     GuessGame(game.game_type, game.difficulty).play(int(game.difficulty))
-# elif game.game_type == "3":
-#     play3(int(game.difficulty))
+elif game.game_type == "3":
+    CurrencyRouletteGame(game.game_type, game.difficulty).play(int(game.difficulty))
 
