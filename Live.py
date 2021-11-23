5c63c20806282a670c4e3da79f6bbdf8ec6ee57c
oberon1984 <89933511+oberon1984@users.noreply.github.com>
2021-11-23 20:21:14 +0200

    update commit

--- Live.py
+++ Live.py
@@ -1,4 +1,5 @@
 from GameClass import Game
+import time
 
 current_name = ""
 game_type = 0
@@ -7,28 +8,42 @@ difficulty = 0
 
 def player_name():
     global current_name
-    current_name = input(f"Please enter your first name: \n")
+    current_name = input(f"Please enter your first name:\n")
     return current_name
 
 
 def welcome(name):
-    print(f"Hello {name} and welcome to World of Games (WoG). \nHere you can find many cool games to play. \n")
+    print(f"Hello {name} and welcome to World of Games (WoG).\nHere you can find many cool games to play.\n")
 
 
 def load_game():
     global game_type
     global difficulty
-    try:
-        while True:
-
-        while not any([game_type == "1", game_type == "2", game_type == "3"]):
-            game_type = input(f"Please choose a game to play: \n"
-                              f"1. Memory Game - a sequence of numbers will appear for 1 second, and you have to "
-                              f"guess it back. \n"
-                              f"2. Guess Game - guess a number and see if you chose like the computer. \n"
-                              f"3. Currency Roulette - try and guess the value of a random amount of USD in ILS. \n")
-    while not any([difficulty == "1", difficulty == "2", difficulty == "3", difficulty == "4", difficulty == "5"]):
-        difficulty = input(f"Please choose game difficulty from 1 to 5:\n")
+    while True:
+        try:
+            game_type = int(input(f"Please choose a game to play:\n"
+                                  f"1. Memory Game - a sequence of numbers will appear for 1 second, and you have to "
+                                  f"guess it back.\n"
+                                  f"2. Guess Game - guess a number and see if you chose like the computer.\n"
+                                  f"3. Currency Roulette - try and guess the value of a random amount of USD in ILS.\n")
+                            )
+            if game_type in range(1, 4):
+                break
+            else:
+                print(f"\nOops, it seems like you hit the wrong number, please try again using numbers between 1-3.\n")
+                time.sleep(2)
+        except ValueError:
+            print(f"\nPlease use only numbers between 1-3. \n")
+            time.sleep(2)
+    while True:
+        try:
+            difficulty = int(input(f"\nPlease choose game difficulty from 1 to 5: \n"))
+            if difficulty in range(1, 6):
+                break
+            else:
+                print(f"\nOops, it seems like you hit the wrong number, please try again using numbers between 1-5.\n")
+                time.sleep(2)
+        except ValueError:
+            print(f"\nPlease use only numbers between 1-5. \n")
+            time.sleep(2)
     return Game(game_type, difficulty)
-
-
