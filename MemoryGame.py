962cbb66c74f20b0ab12de68b9aa547e6183631c
oberon1984 <89933511+oberon1984@users.noreply.github.com>
2021-11-27 18:12:53 +0200

    update commit

--- MemoryGame.py
+++ MemoryGame.py
@@ -21,19 +21,27 @@ def generate_sequence(difficulty):
 
 def get_list_from_user(difficulty):
     user_num_list = []
-    for i in range(1, difficulty + 1):
-        user_num = input(f"Please enter one of the numbers you remember, and press 'Enter':\n")
-        if user_num.isnumeric():
-            user_num_list.append(int(user_num))
-        else:
-            print(f"\nPlease try again, make sure to use numbers only.\n")
-            time.sleep(2)
-            break
+    is_answer_correct = False
+    while not is_answer_correct:
+        for i in range(1, difficulty + 1):
+            user_num = input(f"Please enter one of the numbers you remember, and press 'Enter':\n")
+            if user_num.isnumeric():
+                user_num_list.append(int(user_num))
+                is_answer_correct = True
+            else:
+                print(f"\nPlease try again, make sure to use numbers only.\n")
+                time.sleep(2)
+                is_answer_correct = False
+                break
     return user_num_list
 
 
 def is_list_equal(random_num_list, user_num_list):
+    is_equal = ""
     for i in user_num_list:
-        check = i in random_num_list
-        is_equal_list = ""
+        is_equal = i in random_num_list
+        if not is_equal:
+            print(is_equal)
+            return is_equal
+        print(is_equal)
     return is_equal
