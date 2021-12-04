ba3179c821af4a25a2ea2d3a80a1e659bdabdc05
oberon1984 <89933511+oberon1984@users.noreply.github.com>
2021-12-04 11:56:56 +0200

    update commit

--- Utils.py
+++ Utils.py
@@ -1,8 +1,17 @@
 import os
+import platform
+import time
 
 SCORES_FILE_NAME = "Scores.txt"
 BAD_RETURN_CODE = 1
 
 
 def screen_cleaner():
-    os.system("clear")
+    if platform.system() == "Linux":
+        os.system("clear")
+    elif platform.system() == "Windows":
+        os.system("cls")
+    elif platform.system() == "Darwin":
+        os.system("/usr/bin/osascript -e")
+    else:
+        pass
