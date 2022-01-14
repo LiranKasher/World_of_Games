45510d7ed9e5250c011de63dfd99ac538432e001
oberon1984 <lirankasher332@hotmail.com>
2022-01-14 15:43:36 +0200

    update commit

--- Score.py
+++ Score.py
@@ -7,9 +7,9 @@ points_of_winning = 0
 def add_score(difficulty):
     global points_of_winning
     latest_points_of_winning = (difficulty * 3) + 5
+    previous_points_of_winning = np.genfromtxt(score_file_name, dtype=int)
+    points_of_winning = previous_points_of_winning + latest_points_of_winning
     with open(score_file_name, "r+") as score:
-        previous_points_of_winning = np.genfromtxt(score_file_name, dtype=int)
-        points_of_winning = previous_points_of_winning + latest_points_of_winning
         score.write(f"{points_of_winning}")
     return points_of_winning
 
